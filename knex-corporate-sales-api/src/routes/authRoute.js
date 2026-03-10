d686e4309451f35c2577768d2a10b990bf3b51b2
Pedro Antônio <pedroantonio.techdev@gmail.com>
2026-03-10 14:17:37 -0300

    feat: Implementação das rotas de registro e login (authRoute.js)

---
+++ knex-corporate-sales-api/src/routes/authRoute.js
@@ -0,0 +1,14 @@
+/* Rota de Autenticação */
+const express = require("express")
+
+const router = express.Router()
+
+const authController = require("../controllers/authController")
+
+/* Rota de Registro */
+router.post("/register", authController.register)
+
+/* Rota de Login */
+router.post("/login", authController.login)
+
+module.exports = router
